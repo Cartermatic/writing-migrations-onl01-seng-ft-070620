4e80450f3324f617f65436a4d573285eb7418c34
Carter Parrett <cparrett94@hotmail.com>
2020-08-12 00:44:18 +0000

    Automatically backed up by Learn

--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -1,6 +1,6 @@
 
 
-class ChangeDatatypeForBirthdate < ActiveRecord::Migration
+class ChangeDatatypeForBirthdate < ActiveRecord::Migration[4.2]
   def change
     change_column(:students, :birthdate, datetime)
   end
